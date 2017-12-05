97cbe3fccf9a9ec8885733e1e5a816c7f36c1412
Virginia Cruz <virginiarcruz@gmail.com>
2017-12-05 09:50:21 -0300

    revisando aula 32, ajax post e get

--- exemplos/challenge-32/front/js/main.js
+++ exemplos/challenge-32/front/js/main.js
@@ -1,13 +1,53 @@
 (function () {
     'user strict';
 
-    var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'localhost:3000/user');
-    ajax.send();
-    ajax.addEventListener('readystatechange', function (e) {
-        if (ajax.readyState === 4 && ajax.status === 200) {
-            console.log(ajax.responseText);
-        }
+
+    // MÉTODO GET
+
+    // var ajax = new XMLHttpRequest();
+    // ajax.open('GET', 'http://localhost:3000/user/maria');
+    // ajax.send();
+
+    // ajax.addEventListener('readystatechange', function (e) {
+    //     // metodo abort 
+    //     // if (ajax.readyState === 2) {
+    //     //     console.log('headers ok');
+    //     //     ajax.abort();
+    //     // }
+
+    //     if (ajax.readyState === 4) {
+    //         console.log(ajax.responseText, ajax.status);
+    //     }
             
-    }, false);
+    // }, false); 
+
+
+    // METODO POST
+
+    var ajax = new XMLHttpRequest();
+    ajax.open('POST', 'http://localhost:3000/user');
+    ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    ajax.send('username=fernando&user=Fernando&age=30');
+
+    console.log('Cadastrando user');
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+            console.log('usuario cadastrado', ajax.response);
+        }
+    };
+
+
+    // METODO GET PARA VERIFICAR se O USUÁRIO EXISTE
+
+    var get = new XMLHttpRequest(); 
+    get.open('GET', 'http://localhost:3000/user/fernando');
+    get.send();
+
+    get.onreadystatechange = function () {
+        if (get.readyState === 4) {
+            console.log(JSON.parse(get.responseText));
+        }
+    };
+
+
 })();
--- exemplos/challenge-32/rest-api/index.js
+++ exemplos/challenge-32/rest-api/index.js
@@ -1,7 +1,40 @@
+// import { userInfo } from 'os';
+
 'use strict';
 
 var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
+
+
+// var users = {
+//     joao : {
+//         username: 'Joao',
+//         age: 30
+//     },
+//     maria : {
+//         username: 'Maria',
+//         age: 35
+//     },
+//     francis : {
+//         username: 'Francis',
+//         age: 37
+//     },
+// }
+
+var users = [{
+    username: 'joao',
+    name: 'joao',
+    age: 35
+}, {
+    username: 'maria ',
+    name: 'Maria',
+    age: 22  
+}];
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.send('<h1>Home</h1>');
@@ -11,8 +44,47 @@ app.get('/user', function (req, res) {
     res.send('User');
 });
 
-app.get('/arroz', function (req, res) {
-    res.send('Arroz');
+// app.get('/user/:username', function(req, res){
+//     var username = req.params.username;
+//     if(users[username]) {
+//         return res.json(users[username]);
+//     }
+//     res.status(404).json({erro: 'usuário nao cadastrado'});
+// });
+
+
+app.get('/user/:username', function(req, res){
+    var username = req.params.username;
+    var hasUser = users.some(function (user) {
+        return user.username === username;
+    });
+
+    if(hasUser) {
+        return res.json(users.filter(function (user) {
+            return user.username === username;
+        })[0]);
+    }
+    res.status(404).json({error: 'user nao cadastrado'});
+});
+
+app.post('/user', function (req, res) {
+    var username = req.body.username;
+    var age = req.body.age;
+    var user = req.body.user;
+
+    var hasUser = users.some(function (user) {
+        return user.username === username;
+    });
+
+    if(!hasUser) {
+        users.push({
+            username: username,
+            age: age,
+            user: user
+        });
+    }
+
+    res.json(users);
 });
 
 app.listen(3000);
